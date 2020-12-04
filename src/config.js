1bd933e54146f38867be0f811c634a0fa1f218da
Jesse Tran <jessetran03@gmail.com>
2020-12-04 10:31:26 -0600

    Fixed bugs

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
-  DATABASE_URL: process.env.DATABASE_URL || 'postgres://wkpibkpdduiruz:5ce6680d5ff8d5a8974c50c17f12519de19e140af653ce4c9e67a59a79b69225@ec2-35-174-88-65.compute-1.amazonaws.com:5432/d639qhtdfodqca',
+  DATABASE_URL: process.env.DATABASE_URL || 'postgres://postgres@localhost:5432/noteful',
 }
